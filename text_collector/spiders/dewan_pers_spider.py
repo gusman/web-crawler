a22fe443b136ce3e523b3e5d54633e0bf4be3c6f
Gusman Dharma P <gusmnan.dp@gmail.com>
2020-02-23 22:00:00 +0700

    Add headers for dewan_pers scraper selenium

--- text_collector/spiders/dewan_pers_spider.py
+++ text_collector/spiders/dewan_pers_spider.py
@@ -10,10 +10,23 @@ class QuotesSpider(scrapy.Spider):
         'https://dewanpers.or.id/data/perusahaanpers',
     ]
     list_links = []
+    headers = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36',
+        'Sec-Fetch-User': '?1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'navigate',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+    }
 
     def start_requests(self):
         for url in self.start_urls:
-            yield SeleniumRequest(url=url, callback=self.parse_initial)
+            yield SeleniumRequest(url=url, callback=self.parse_initial, headers=self.headers)
 
     def parse_initial(self, response):
         driver = response.request.meta['driver']
