9ddf858e4bd315081b552480072f95ccffc67a24
Gusman Dharma P <gusmnan.dp@gmail.com>
2020-02-25 00:23:57 +0700

    Add sumbar_haluan spider

---
+++ text_collector/spiders/sumbar_haluan.py
@@ -0,0 +1,40 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class ItemNews(scrapy.Item):
+    date = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+
+class SinggalangSpider(CrawlSpider):
+    stop_flag = False
+    name = "sumbar_haluan"
+    allowed_domains = [ 'harianhaluan.com' ]
+    start_urls = [
+        'https://www.harianhaluan.com/news',
+    ]
+   
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=('news/detail', )
+            ), 
+            callback='parse_detail'
+        ),
+        Rule(
+            LinkExtractor(
+                    allow=('news/kanal', ),
+                    deny=('news/sub', 'news/indeks', 'news/popular')
+            ),
+        )
+    )
+
+    def parse_detail(self, response):
+        item = ItemNews()
+        item['date'] = response.xpath('//div[@class="date fl"]/text()').get()
+        item['title'] = response.xpath('//h1[@class="l_blue2_detik lbold"]/text()').get()
+        content = response.xpath('//div[@itemprop="articleBody"]/p/text()').getall()
+        item['content']  = "".join(content)
+        yield item
