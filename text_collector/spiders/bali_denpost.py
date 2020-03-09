909759101859a89a049cd8b9a98efd61b42cc79b
Gusman Dharma Putra <gusman.dharma_putra@nokia.com>
2020-03-09 17:47:50 +0700

    Add bali_denpost spider

---
+++ text_collector/spiders/bali_denpost.py
@@ -0,0 +1,42 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class ItemNews(scrapy.Item):
+    date = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+
+class JurnalSumbarSpider(CrawlSpider):
+    stop_flag = False
+    name = "bali_denpost"
+    allowed_domains = [ 'denpost.id' ]
+    start_urls = [
+        'https://denpost.id/',
+    ]
+   
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=('[0-9]{4}/[0-9]{2}', )
+            ), 
+            callback='parse_detail'
+        ),
+        Rule(
+            LinkExtractor(
+                    #allow=('category', 'tag', 'page'),
+                    deny=('iklan-kolom', 'info-denpost', 'iklan', \
+                        'pedoman-media-siber'),
+            ),
+        )
+    )
+
+    def parse_detail(self, response):
+        self.logger.info('\n >> PROCESSING in parse_detail %s\n', response.url)
+        item = ItemNews()
+        item['title'] = response.xpath('//h1[@class="entry-title"]/text()').get()
+        item['date'] = response.xpath('//time[@class="entry-date updated td-module-date"]/text()').get()
+        content = response.xpath('//article//p//text()').getall()
+        item['content']  = "".join(content)
+        yield item
